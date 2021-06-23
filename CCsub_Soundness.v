cdfa9590b7aa92b9c7f96cde1374e591b42bacf3
Edward Lee <e45lee@uwaterloo.ca>
2021-06-23 11:49:23 -0400

    wip: remove narrowing for context typing.

--- CCsub_Soundness.v
+++ CCsub_Soundness.v
@@ -86,180 +86,6 @@ Proof with eauto.
   - exists C. exists P...
 Qed.
 
-Lemma ctx_typing_narrowing : forall E T e S,
-  E |-ctx e ~: T ->
-  sub E S T ->
-  E |-ctx e ~: S.
-Proof with eauto.
-  intros * Typ Sub. generalize dependent S. 
-  dependent induction Typ; intros S Sub.
-  - Case "top".
-    constructor...
-  - Case "KFun".
-    dependent induction Sub...
-    {
-      econstructor...
-    }
-    inversion select (sub_pre _ _ _); subst.
-    econstructor.
-    + apply H.
-    + apply (sub_transitivity T1)...
-    + apply IHTyp...
-      pick fresh x.
-      replace (open_ct S2 (cv T1'))
-        with (subst_ct x (cv T1') (open_ct S2 (`cset_fvar` x))).
-      2: {
-        rewrite subst_ct_open_ct.
-        3: notin_solve.
-        2: eapply capt_from_wf_cset; eapply (cv_wf E)...
-        f_equal.
-        2: csetdec.
-        symmetry; apply subst_ct_fresh.
-        notin_solve.
-      }
-      replace (open_ct T2 (cv T1'))
-        with (subst_ct x (cv T1') (open_ct T2 (`cset_fvar` x))).
-      2: {
-        rewrite subst_ct_open_ct.
-        3: notin_solve.
-        2: eapply capt_from_wf_cset; eapply (cv_wf E)...
-        f_equal.
-        2: csetdec.
-        symmetry; apply subst_ct_fresh.
-        notin_solve.
-      }
-      rewrite_env ((map (subst_cb x (cv T1')) empty) ++ E).
-      apply (sub_through_subst_ct x T1).
-      2: apply sub_implies_subcapt...
-      simpl_env; apply H12; notin_solve.
-  - Case "KArg".
-    econstructor...
-    1: apply (sub_transitivity T1')...
-    assert (wf_pretyp_in E (typ_arrow T1 T2)) as HA by eauto.
-    inversion HA; subst.
-    apply IHTyp...
-    inversion HA; subst.
-    pick fresh x.
-    replace (open_ct T2 (cv S))
-      with (subst_ct x (cv S) (open_ct T2 (`cset_fvar` x))).
-    2: {
-      rewrite subst_ct_open_ct.
-      3: notin_solve.
-      2: eapply capt_from_wf_cset; eapply (cv_wf E)...
-      f_equal.
-      2: csetdec.
-      symmetry; apply subst_ct_fresh.
-      notin_solve.
-    }
-    replace (open_ct T2 (cv T1'))
-      with (subst_ct x (cv T1') (open_ct T2 (`cset_fvar` x))).
-    2: {
-      rewrite subst_ct_open_ct.
-      3: notin_solve.
-      2: eapply capt_from_wf_cset; eapply (cv_wf E)...
-      f_equal.
-      2: csetdec.
-      symmetry; apply subst_ct_fresh.
-      notin_solve.
-    }
-    enough (sub ([(x, bind_typ T1)] ++ E)
-                (subst_ct x (cv S) (open_ct T2 (`cset_fvar` x)))
-                (subst_ct x (cv T1') (open_ct T2 (`cset_fvar` x)))) as HE. {
-      rewrite_env (empty ++ [(x, bind_typ T1)] ++ E) in HE.
-      forwards HP: sub_through_subst_ct (cv T1) HE. {
-        eapply subcapt_reflexivity.
-        - eapply cv_wf...
-        - csetdec.
-      }
-      simpl_env in HP.
-      assert (x `notin` (`cset_fvars` (cv S))). {
-        assert (wf_cset_in E (cv S)) as HAA by (apply cv_wf;eauto).
-        inversion HAA; subst; simpl_env in *.
-        assert (x `notin` dom E) by notin_solve.
-        intros ?.
-        (* How come this doesn't work ??? *)
-        (* assert (x `in`A {}A) by fsetdec. *)
-        assert ({x}A `c`A fvars) by fsetdec.
-        assert ({x}A `c`A dom E) as HSubSet by fsetdec.
-        fsetdec.
-      }
-      assert (x `~in`A `cset_fvars` (cv T1')). {
-        assert (wf_cset_in E (cv T1')) as HAA by (apply cv_wf;eauto).
-        inversion HAA; subst; simpl_env in *.
-        assert (x `notin` dom E) by notin_solve.
-        intros ?.
-        (* How come this doesn't work ??? *)
-        (* assert (x `in`A {}A) by fsetdec. *)
-        assert ({x}A `c`A fvars) by fsetdec.
-        assert ({x}A `c`A dom E) as HSubSet by fsetdec.
-        fsetdec.
-      }
-      repeat (rewrite subst_ct_useless_repetition in HP by notin_solve).
-      apply HP.
-    }
-    assert (wf_env ([(x, bind_typ T1)] ++ E)) by (constructor;eauto).
-    applys plain_subst_ct_monotonicity; simpl_env.
-    5: apply H10.
-    all: simpl_env;eauto.
-    1: eapply type_from_wf_typ...
-    1: apply sub_implies_subcapt;
-      rewrite_env (empty ++ [(x, bind_typ T1)] ++ E);
-      apply sub_weakening;simpl_env...
-    1: {
-      assert (wf_cset_in E (cv S)) as WfCvS by (apply cv_wf;eauto).
-      rewrite_env (empty ++ E) in WfCvS.
-      rewrite_env (empty ++ [(x, bind_typ T1)] ++ E).
-      applys wf_cset_weakening WfCvS; simpl_env...
-    }
-    1: {
-      assert (wf_cset_in E (cv T1')) as WfCvT1' by (apply cv_wf;eauto).
-      rewrite_env (empty ++ E) in WfCvT1'.
-      rewrite_env (empty ++ [(x, bind_typ T1)] ++ E).
-      applys wf_cset_weakening WfCvT1'; simpl_env...
-    }
-  - Case "KTyp".
-    dependent induction Sub... {
-      econstructor...
-    }
-    inversion select (sub_pre _ _ _); subst.
-    econstructor.
-    1: apply (sub_transitivity T1)...
-    apply IHTyp...
-    pick fresh x.
-    replace (open_tt S2 T) with (subst_tt x T (open_tt S2 x)).
-    2: {
-      rewrite subst_tt_open_tt...
-      f_equal.
-      2: unfold subst_tt; destruct (x == x); easy.
-      symmetry; apply subst_tt_fresh.
-      notin_solve.
-    }
-    replace (open_tt T2 T) with (subst_tt x T (open_tt T2 x)).
-    2: {
-      rewrite subst_tt_open_tt...
-      f_equal.
-      2: unfold subst_tt; destruct (x == x); easy.
-      symmetry; apply subst_tt_fresh.
-      notin_solve.
-    }
-    rewrite_env ((map (subst_tb x T) empty) ++ E).
-    apply sub_through_subst_tt with (Z := x) (Q := T1)...
-  - Case "HReset".
-    eapply typing_ctx_reset.
-    admit.
-  - case "KThrowHandler".
-    admit.
-  - Case "KThrowArg".
-    admit.
-(*
-  - Case "Ctx-Var".
-    apply IHTyp.
-    eapply sub_transitivity...
-    eapply sub_trans_tvar...
-    eapply sub_reflexivity with (Ap := dom E) (Am := dom E)...
-    apply wf_typ_from_binds_sub in H... *)
-Admitted.
-
 Lemma preservation : forall e e',
   typing_state e ->
   step e e' ->
@@ -270,24 +96,24 @@ Proof with simpl_env; eauto.
   - inversion Typ; subst.
     dependent induction H2. 2: {
       eapply IHtyping...
-      eapply ctx_typing_narrowing...
+      eapply typing_ctx_sub...
     }
     econstructor...
     econstructor...
   - inversion Typ; subst.
     dependent induction H2. 2: {
       eapply IHtyping...
-      eapply ctx_typing_narrowing...
+      eapply typing_ctx_sub...
     }
     econstructor...
     econstructor...
   - inversion Typ; subst.
-    inversion H2; subst.
+    dependent induction H2...
     econstructor...
     econstructor...
   - inversion Typ; subst.
-    inversion H2; subst.
-    dependent induction H5. 2: {
+    dependent induction H2...
+  dependent induction H5. 2: {
       inversion select (sub empty S _); subst. {
         inversion select (binds _ _ _).
       }
