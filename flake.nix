d3051183ecceaab58e717830d644b7be2b5ccc55
Joseph Fourment <joseph.fourment@epfl.ch>
2022-11-04 13:37:31 +0100

    setup coq-dpdgraph

--- flake.nix
+++ flake.nix
@@ -8,11 +8,26 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+        coq = pkgs.coq_8_10;
+        coq-dpdgraph = (pkgs.coqPackages_8_10.dpdgraph.overrideAttrs (p: p // {
+          src = pkgs.fetchFromGitHub {
+            owner = "coq-community";
+            repo = "coq-dpdgraph";
+            rev = "v0.6.6";
+            sha256 = "sha256-HDXJcjDz8WzSBLkDIZTWjOijcByr5MajjozwL3+pWb4=";
+          };
+        }));
       in
       {
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
-            coq_8_10
+            coq
+            coq-dpdgraph
+            (python310.withPackages (pyPkgs: with pyPkgs; [
+              fire
+              python-lsp-server
+            ]))
           ];
         };
       }
